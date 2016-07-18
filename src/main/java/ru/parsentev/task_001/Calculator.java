08d28c63ade5f8b6a4ff4209fb66f6bab84e0989
Petr Arsentev <arsentyev.petr@otr.ru>
2016-07-18 12:59:16 +0300

    added maven and added desc

---
+++ src/main/java/ru/parsentev/task_001/Calculator.java
@@ -0,0 +1,16 @@
+package ru.parsentev.task_001;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * TODO: comment
+ *
+ * @author parsentev
+ * @since 18.07.2016
+ */
+public class Calculator {
+    private static final Logger log = getLogger(Calculator.class);
+}
---
+++ src/main/java/ru/parsentev/task_001/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * TODO: comment
+ *
+ * @author parsentev
+ * @since 18.07.2016
+ */
+package ru.parsentev.task_001;
