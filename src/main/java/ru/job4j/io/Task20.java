53400b269edd1994f7ab0766b612a8154f5a1dc2
Denis Kitrish <denis.kitrish@outlook.com>
2022-08-14 18:13:54 +0300

    Раздел 2. Команды вывода. Команды ввода Задание 2.11 Смещён комментарий // YOUR CODE ниже

--- src/main/java/ru/job4j/io/Task20.java
+++ src/main/java/ru/job4j/io/Task20.java
@@ -4,6 +4,7 @@ public class Task20 {
     public static void main(String[] args) {
         String fullName = "Фамилия Имя Отчество";
         String phoneNumber = "+7(938) 123-45-67";
+        // YOUR CODE
         int maxLenght = Math.max(fullName.length(), phoneNumber.length());
         String topBorders = "*".repeat(maxLenght + 4);
         String bottomBorders = topBorders;
